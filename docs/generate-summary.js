c28d370ba5ae9dd496186fdf9b307b5a4335c7ce
Nathan Cuel Oller OpenEdition <nathan.cuel-oller@openedition.org>
2026-01-16 09:45:32 +0100

    Update generate-summary.js

--- docs/generate-summary.js
+++ docs/generate-summary.js
@@ -16,10 +16,6 @@ function getTitle(filePath) {
 function walk(dir, level = 1) {
   let result = "";
   const files = fs.readdirSync(dir).sort();
-
-if (files.includes("README.md")) {
-    files = ["README.md", ...files.filter(f => f !== "README.md")];
-  }
   
   for (const file of files) {
     if (file.startsWith(".")) continue;
